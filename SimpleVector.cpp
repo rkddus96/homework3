9e5f5fd6a67a81ba13d7da89f98a57ea279ea55f
rkddus96 <103873698+rkddus96@users.noreply.github.com>
2024-12-30 20:03:30 +0900

    Initial commit

---
+++ SimpleVector.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class SimpleVector
+{
+private:
+    T* data;
+    int currentSize;
+    int currentCapacity;
+
+public:
+    void resize(int newCapacity)
+    {
+        if (newCapacity > currentCapacity)
+        {
+            T* newData = new T[newCapacity];
+            for (int i = 0; i < currentSize; i++)
+            {
+                newData[i] = data[i];
+            }
+            delete[] data;
+            data = newData;
+            currentCapacity = newCapacity;
+        }
+
+    }
+
+    SimpleVector() : currentSize(0), currentCapacity(10)
+    {
+        data = new T[currentCapacity];
+    }
+
+    SimpleVector(int capacity) : currentSize(0), currentCapacity(capacity)
+    {
+        data = new T[currentCapacity];
+    }
+
+    SimpleVector(const SimpleVector& other) : currentSize(other.currentSize), currentCapacity(other.currentCapacity)
+    {
+        data = new T[currentCapacity];
+        for (int i = 0; i < currentsize; i++)
+        {
+            data[i] = other.data[i];
+        }
+    }
+
+    ~SimpleVector()
+    {
+        delete[] data;
+    }
+
+    void push_back(const T& value)
+    {
+        if (currentSize == currentCapacity)
+        {
+            currentCapacity = 5 + currentCapacity;
+            resize(currentCapacity);
+        }
+
+        data[currentSize++] = value;
+    }
+
+    void pop_back()
+    {
+        if (currentSize != 0)
+        {
+            currentSize--;
+        }
+
+    }
+
+    int size() const 
+    {
+        return currentSize;
+    }
+
+    int capacity() const 
+    {
+        return currentCapacity;
+    }
+
+    void sortData()
+    {
+        sort(data, data + currentSize);
+    }
+
+};
+
+int main()
+{
+
+}
